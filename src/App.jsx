a815357030840ed4ede7d0425280764196fff828
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-04-23 18:52:24 +0200

    Created and added a navigation component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navigation from './components/Navigation';
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navigation />
+
+    </div>
+  );
+}
+
+export default App;
